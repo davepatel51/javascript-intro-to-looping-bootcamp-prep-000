71ec26e57c66bdbaee524516eae0210504123c2f
Dave Patel <ddp2134@gmail.com>
2020-02-17 06:17:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,26 +9,3 @@ function forLoop(array){
 return array
 }
 
-var sample_arr = [];
-console.log(forLoop(sample_arr));
-
-
-function whileLoop(n){
-  while(n>0)
-  {console.log(n--)}
-  return "done"
-}
-
-
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-function doWhileLoop(array){
-do {
-  array.pop()
-} while (array.length>0 && maybeTrue())
-return array
-}
-
-do_while_arr = ['a','b','c'];
-console.log(doWhileLoop(do_while_arr))
